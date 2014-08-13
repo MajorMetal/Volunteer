399b5a292cc6622e033f90741ff46d5051223a44
theDavid <David@awesome.com>
2014-08-13 13:48:12 -0700

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813003141) do
+ActiveRecord::Schema.define(version: 20140813201646) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20140813003141) do
     t.string   "image"
     t.string   "contact_phone"
     t.string   "contact_email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "zip_code"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
