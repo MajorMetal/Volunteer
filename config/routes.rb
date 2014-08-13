e2f8075782cf8f229b9b4a8a3513ae1584e9866a
theDavid <David@awesome.com>
2014-08-12 18:39:36 -0700

    created routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+
+  get 'events' => 'events#index'
+
+  get 'events/new'
+
+  get 'events/create'
+
+  get 'events/show'
+
+  get 'events/edit'
+
+  get 'events/update'
+
+  get 'events/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
